e1adecc3c174d1a467484962ab3557cc41070029
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-03-03 22:32:29 +0100

    Added base routes

--- src/main/scala/nl/rafaelklaessen/scalatrablog/ScalatraBlogApiServlet.scala
+++ src/main/scala/nl/rafaelklaessen/scalatrablog/ScalatraBlogApiServlet.scala
@@ -11,8 +11,24 @@ class ScalatraBlogApiServlet extends ScalatraBlogStack with JacksonJsonSupport {
     contentType = formats("json")
   }
 
-  post("/") {
+  post("user/login") {
+
+  }
+
+  case class Error(error_description: String) 
+
+  post("/post/put") {
+    val title: String = params.getOrElse("title", halt(400, Error("Please provide a title")))
+    val content: String = params.getOrElse("content", halt(400, Error("Please provide content")))
+    
+    
+    println(request.isAjax)
+    println(params.get("henkisniethier"))
     List("json", "example")
+  }
+
+  post("/post/delete") {
+
   }
 
 }
